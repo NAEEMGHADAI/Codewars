04f11aa8a17eaaa4bc58c43de4a1f1e6534210c4
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-10-08 21:02:56 +0530

    Alternate soln for same problem

--- 7kyu/Sorted yes no how.js
+++ 7kyu/Sorted yes no how.js
@@ -1,18 +1,54 @@
+// Sorted? yes? no? how?
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
 //PREP
 //Parameter: array of number, can have negative number
 //return yes asc,yes dec, no
 
 function isSortedAndHow(array) {
-	let ascending = array.filter((e, i, a) => e > a[i + 1]).length == 0;
-	let descending = array.filter((e, i, a) => e < a[i + 1]).length == 0;
-	console.log(ascending, descending);
-	if (!ascending && !descending) {
+	let asc = 0;
+	let desc = 0;
+	for (let index = 0; index < array.length - 1; index++) {
+		if (array[index] <= array[index + 1]) {
+			asc = 1;
+		} else {
+			asc = 0;
+			break;
+		}
+	}
+	if (asc !== 1) {
+		for (let index = 0; index < array.length - 1; index++) {
+			if (array[index] >= array[index + 1]) {
+				desc = 1;
+			} else {
+				desc = 0;
+				break;
+			}
+		}
+	}
+
+	if (!asc && !desc) {
 		return "no";
-	} else if (ascending) {
+	} else if (asc) {
 		return "yes, ascending";
 	} else {
 		return "yes, descending";
 	}
+	// let ascending = array.filter((e, i, a) => e > a[i + 1]).length == 0;
+	// let descending = array.filter((e, i, a) => e < a[i + 1]).length == 0;
+	// console.log(ascending, descending);
+	// if (!ascending && !descending) {
+	// 	return "no";
+	// } else if (ascending) {
+	// 	return "yes, ascending";
+	// } else {
+	// 	return "yes, descending";
+	// }
 }
 console.log(isSortedAndHow([1, 2])); //yes asc
 console.log(isSortedAndHow([15, 7, 3, -8])); //yes desc
