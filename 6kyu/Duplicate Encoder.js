f0fa7f263d9625e8aacad1e93192c3d345eeabc5
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-10-28 12:38:55 +0530

    6&7 Kyu Questions (28 Oct 2022)

---
+++ 6kyu/Duplicate Encoder.js
@@ -0,0 +1,38 @@
+// Duplicate Encoder
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))(("
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+function duplicateEncode(word) {
+	word = word.toLowerCase();
+	let obj = {};
+	for (let i = 0; i < word.length; i++) {
+		if (obj[word[i]]) {
+			obj[word[i]]++;
+		} else {
+			obj[word[i]] = 1;
+		}
+	}
+
+	let ans = "";
+	for (let i = 0; i < word.length; i++) {
+		if (obj[word[i]] > 1) {
+			ans += ")";
+		} else {
+			ans += "(";
+		}
+	}
+	return ans;
+}
+
+console.log(duplicateEncode("din")); //(((
+console.log(duplicateEncode("recede")); //()()()
+console.log(duplicateEncode("Success")); //)())())
+console.log(duplicateEncode("(( @")); //))((
